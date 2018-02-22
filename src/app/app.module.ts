aeeaaa99c6eeab940c0192b2630a3db711ed7a1f
artes07 <artes07333@gmail.com>
2018-02-22 19:14:28 +0200

    add routerModule and links without refreshing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { AppComponent } from './app.component';
 import { CarsPageComponent } from './cars-page/cars-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import {CarsService} from './cars.service';
-
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,10 @@ import {CarsService} from './cars.service';
     HomePageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [CarsService],
   bootstrap: [AppComponent]
